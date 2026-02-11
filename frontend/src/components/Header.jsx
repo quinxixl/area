394f407b5e0c28024c34c59b4197f29e10f95c98
Никита <nikita@MacBook-Pro-Nikita.local>
2026-02-11 08:39:11 +0300

    Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import logo from '../resources/logo.svg'
+import '../styles/header.scss'
+
+export default function Header() {
+    return(
+        <div className="header">
+            <img src = {logo} className="header-logo"/>
+            <nav className='header-nav'>
+                <ul className='header-nav-list'>
+                    <li><a>Benefits</a></li>
+                    <li><a>Specifications</a></li>
+                    <li><a>How-to</a></li>
+                    <li><a>Contact Us</a></li>
+                </ul>
+            </nav>
+            <div className='header-cta'>
+                <a>Learn More</a>
+            </div>
+        </div>
+    );
+}
